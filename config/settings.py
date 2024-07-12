020ab4bea3b3cf659898a4eb61091a95334a39c1
Giulliana Beatriz <giullianabeatriz13@gmail.com>
2024-07-12 10:16:26 -0300

    aula dia 12/07

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = [ 
+    'tarefas',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
